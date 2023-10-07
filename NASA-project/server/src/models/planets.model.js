3bc65b53f00655227e158ebf0fef1eb049266609
Madalin <turluianu.petre.madalin@gmail.com>
2023-10-07 15:23:59 +0300

    .

--- NASA-project/server/src/models/planets.model.js
+++ NASA-project/server/src/models/planets.model.js
@@ -1,3 +1,34 @@
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const path = require("path");
+
 const planets = [];
 
-module.exports = planets;
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.35 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+async function loadPlanetsData() {
+  fs.createReadStream(
+    path.join(__dirname, "..", "..", "data", "kepler.data.csv")
+  )
+    .pipe(
+      parse({
+        comment: "#",
+        columns: true,
+      })
+    )
+    .on("data", (data) => {
+      if (isHabitablePlanet(data)) planets.push(data);
+    })
+    .on("error", (error) => {
+      throw new Error(`ERROR in models/planets.models.js: ${error}`);
+    });
+}
+
+module.exports = { loadPlanetsData, planets };
