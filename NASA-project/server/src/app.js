f9f864d7e91aefa759443b7740118ff440ae2270
Madalin <turluianu.petre.madalin@gmail.com>
2023-10-07 20:29:52 +0300

    .

--- NASA-project/server/src/app.js
+++ NASA-project/server/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const planetsRouter = require("./routes/planets/planets.route");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(
@@ -9,6 +10,11 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
