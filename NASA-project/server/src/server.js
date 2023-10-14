7233783c211e59cec4b5c0d6cc6312fedbc4bb09
Madalin <turluianu.petre.madalin@gmail.com>
2023-10-14 17:15:20 +0300

    .

--- NASA-project/server/src/server.js
+++ NASA-project/server/src/server.js
@@ -1,12 +1,24 @@
 const http = require("http");
 const app = require("./app");
+const mongoose = require("mongoose");
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URL =
+  "mongodb+srv://nasa-api:q1XQHP6nDV3ixp6N@nasa.nujzp4d.mongodb.net/?retryWrites=true&w=majority";
 
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("connection ready");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.error(error);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
